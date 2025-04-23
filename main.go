7652ad037b1286d1d5b6835f36d636ba621827c7
Kirill Ermolov <bezzubikart@yandex.ru>
2025-04-23 21:46:39 +0300

    decompose on functions

--- main.go
+++ main.go
@@ -5,15 +5,41 @@ import (
 	"math"
 )
 
+const IMTPow = 2
+
+var userHeight float64
+var userWeight float64
+
 func main() {
-	const IMTPow = 2
-	var userHeight float64
-	var userWeight float64
+	getUserInput()
+	IMT := calculateIMT(userWeight, userHeight, IMTPow)
+	outputResult(IMT)
+}
+
+func outputResult(IMT float64) {
+	switch {
+	case IMT < 16:
+		fmt.Println("Too low weight")
+	case (16 <= IMT) && (IMT < 18.5):
+		fmt.Println("Low weight")
+	case (18.5 <= IMT) && (IMT < 25):
+		fmt.Println("Normal weight")
+	case (25 <= IMT) && (IMT < 30):
+		fmt.Println("High weight")
+	default:
+		fmt.Println("Too high weight")
+	}
+
+	fmt.Printf("Your IMT is %.1f", IMT)
+}
+
+func calculateIMT(userKG, userHeight, pow float64) float64 {
+	return userKG / math.Pow(userHeight/100, pow)
+}
+
+func getUserInput() {
 	fmt.Print("Type your height in cm: ")
 	fmt.Scanln(&userHeight)
 	fmt.Print("Type your weight in kg: ")
 	fmt.Scanln(&userWeight)
-	IMT := userWeight / math.Pow(userHeight/100, IMTPow)
-	fmt.Printf("Your IMT is %.1f", IMT)
-
 }
