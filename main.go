9d8f04b3df39ee1cc390dc60c9ba524b9d53b8e7
Kirill Ermolov <bezzubikart@yandex.ru>
2025-05-05 12:41:36 +0300

    Added base functions
    Added main loop, input and repeat

--- main.go
+++ main.go
@@ -1,3 +1,49 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"strconv"
+)
+
+var transactions []float64
+
+func main() {
+	fmt.Println("Hello, lets calculate your transactions!\nType positive or negative value of transaction, with point if its needed:\nAfter last input enter empty string")
+	for {
+		inputTransaction()
+		fmt.Println("Do you want to repeat calculations?")
+		if repeatInput() == true {
+			continue
+		}
+		break
+	}
+	fmt.Println(transactions)
+
+}
+
+func inputTransaction() {
+	var transaction string
+	for {
+		fmt.Println("Enter your transaction (n for exit): ")
+		fmt.Scanln(&transaction)
+		if transaction == "n" || transaction == "N" {
+			fmt.Println("End of input")
+			break
+		} else if number, err := strconv.ParseFloat(transaction, 64); err == nil {
+			transactions = append(transactions, number)
+		} else {
+			fmt.Println("Invalid input, please try again")
+			continue
+		}
+	}
+}
+
+func repeatInput() bool {
+	var input string
+	fmt.Scanln(&input)
+	if input == "y" || input == "Y" {
+		return true
+	} else {
+		return false
+	}
+}
