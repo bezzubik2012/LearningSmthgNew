8e4c50912035bb998cb8b4c11b33bd4ca96bacb7
Kirill Ermolov <bezzubikart@yandex.ru>
2025-05-31 15:41:37 +0300

    Main
    Added structure, methods, password generator

--- main.go
+++ main.go
@@ -1,3 +1,45 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"math/rand/v2"
+)
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_-")
+
+type account struct {
+	login    string
+	password string
+	url      string
+}
+
+func main() {
+	login := promptData("Enter your login: ")
+	url := promptData("Enter your URL: ")
+
+	user1 := account{
+		login: login,
+		url:   url,
+	}
+	user1.generatePassword(50)
+	user1.viewAccData()
+}
+
+func promptData(prompt string) string {
+	fmt.Print(prompt)
+	var data string
+	fmt.Scan(&data)
+	return data
+}
+
+func (user *account) viewAccData() {
+	fmt.Println(user.login, user.password, user.url)
+}
+
+func (user *account) generatePassword(lengthPass int) {
+	password := make([]rune, lengthPass)
+	for i := range password {
+		password[i] = letters[rand.IntN(len(letters))]
+	}
+	user.password = string(password)
+}
