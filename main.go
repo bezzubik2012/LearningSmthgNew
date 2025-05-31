a18dc6759ee97f90f383162a61ad4ea67ad2050c
Kirill Ermolov <bezzubikart@yandex.ru>
2025-05-31 16:45:52 +0300

    Added errors
    added errors on login and pass, add auto-generate pass if pass empty

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand/v2"
+	url2 "net/url"
 )
 
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_-")
@@ -15,11 +17,13 @@ type account struct {
 
 func main() {
 	login := promptData("Enter your login: ")
+	password := promptData("Enter your password: ")
 	url := promptData("Enter your URL: ")
 
-	user1 := account{
-		login: login,
-		url:   url,
+	user1, err := newAccount(login, password, url)
+	if err != nil {
+		println("Invalid parameters")
+		return
 	}
 	user1.generatePassword(50)
 	user1.viewAccData()
@@ -28,7 +32,7 @@ func main() {
 func promptData(prompt string) string {
 	fmt.Print(prompt)
 	var data string
-	fmt.Scan(&data)
+	fmt.Scanln(&data)
 	return data
 }
 
@@ -42,4 +46,24 @@ func (user *account) generatePassword(lengthPass int) {
 		password[i] = letters[rand.IntN(len(letters))]
 	}
 	user.password = string(password)
+}
+
+func newAccount(login, password, url string) (*account, error) {
+	newAcc := account{
+		login:    login,
+		password: password,
+		url:      url}
+
+	if login == "" {
+		return nil, errors.New("empty login")
+	}
+	if password == "" {
+		newAcc.generatePassword(50)
+		return &newAcc, nil
+	}
+	_, err := url2.ParseRequestURI(url)
+	if err != nil {
+		return nil, errors.New("invalid URL")
+	}
+	return &newAcc, nil
 }
